5d7a18160b5f431e8a7a50967bd9ddf493b33b66
David Hasenjaeger <davehasen@gmail.com>
2015-04-10 14:21:59 -0700

    starting to add commenting to products

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -3,10 +3,25 @@ var catalogueApp = angular.module('catalogueApp', []);
 catalogueApp.controller('CatalogueCtrl', [ '$http', function($http) {
   this.products = [
     {'name': 'Product 1',
-     'description': 'This is the first product'},
+     'description': 'This is the first product',
+     'comments': []
+    },
     {'name': 'Product 2',
-     'description': 'This is the second product'},
+     'description': 'This is the second product',
+     'comments': []
+    },
     {'name': 'Product 3',
-     'description': 'This is the third product'}
+     'description': 'This is the third product',
+     'comments': []
+    }
   ];
+
+  this.comment = '';
+
+  var self = this;
+
+  this.addComment = function (list) {
+    list.push(self.comment);
+    self.comment = '';
+  }
 }]);
