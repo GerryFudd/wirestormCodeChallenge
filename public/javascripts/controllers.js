c3e328ec779010422eadfeb96468a13bc4068c15
David Hasenjaeger <davehasen@gmail.com>
2015-04-10 15:00:24 -0700

    the hour is up

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,32 +1,41 @@
 var catalogueApp = angular.module('catalogueApp', []);
 
 catalogueApp.controller('CatalogueCtrl', [ '$http', function($http) {
-  this.products = [
-    {'name': 'Product 1',
-     'description': 'This is the first product',
-     'comments': []
-    },
-    {'name': 'Product 2',
-     'description': 'This is the second product',
-     'comments': []
-    },
-    {'name': 'Product 3',
-     'description': 'This is the third product',
-     'comments': []
-    }
-  ];
 
   this.comment = '';
-
+  // self will act as the $scope throughout this controller
   var self = this;
 
-  this.addComment = function (product) {
-    product.comments.push(self.comment);
-    self.comment = '';
-    delete product.showForm;
+  // retrieve the list of products from the database
+  function initiatePage () {
+    $http.get('/api/').
+      success(getList).
+      error(function(data, status, headers, config){
+      });
+  }
+  function getList (data) {
+    self.products = data.products;
   }
 
+  // Controlls the addition of a new comment to a product
+  this.addComment = function (product) {
+    $http.post('/api/addComment',
+      {comment: self.comment, ind: self.products.indexOf(product)}).
+    success(function (data, staus, headers, config) {
+      console.log('added comment: ' + data);
+    }).
+    error(function(data, status, headers, config){
+      console.log(data);
+    });
+    self.comment = '';
+    delete product.showForm;
+    initiatePage();
+  }
+
+  // makes a comment form appear below a product
   this.makeItShow = function (product) {
     product.showForm = true;
   }
+
+  initiatePage();
 }]);
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+// This data mimics the data that would be stored in a database.
+// If I had more time, I would set up a database and access this information through
+// calls to a database.
+var data = [
+  {'name': 'Product 1',
+   'description': 'This is the first product',
+   'comments': []
+  },
+  {'name': 'Product 2',
+   'description': 'This is the second product',
+   'comments': []
+  },
+  {'name': 'Product 3',
+   'description': 'This is the third product',
+   'comments': []
+  }
+];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json({"response": "response text"});
+  res.json({"products": data});
+});
+
+// adds new comments to an existing product
+router.post('/addComment', function(req, res) {
+	var newComment = req.body.comment;
+	var product = data[req.body.ind];
+	product.comments.push(newComment);
+	res.json(newComment);
 });
 
 module.exports = router;
