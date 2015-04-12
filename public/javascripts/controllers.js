6b9ad4cd0ce4975bd3f36835004719761a74dd29
David Hasenjaeger <davehasen@gmail.com>
2015-04-12 14:44:22 -0700

    The site interacts with an actual database and new products may be added.

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,20 +1,40 @@
 var catalogueApp = angular.module('catalogueApp', []);
 
-catalogueApp.controller('CatalogueCtrl', [ '$http', function($http) {
+catalogueApp.controller('CatalogueCtrl',
+  ['$http', function($http) {
+  
+  // some data to be collected from the add product form
+  this.productName = '';
+  this.productCost = '';
+  this.productDescription = '';
 
+  // a datum to be collected from the add comment form
   this.comment = '';
+
   // self will act as the $scope throughout this controller
   var self = this;
 
-  // retrieve the list of products from the database
-  function initiatePage () {
-    $http.get('/api/').
-      success(getList).
-      error(function(data, status, headers, config){
-      });
+  this.showForm = function () {
+    self.showProductForm = true;
   }
-  function getList (data) {
-    self.products = data.products;
+
+  // Controlls the addition of a new comment to a product
+  this.addProduct = function (product) {
+    $http.post('/api/addProduct',
+      {
+        productName: self.productName,
+        productCost: self.productCost,
+        productDescription: self.productDescription
+      }).
+    success(function (data, staus, headers, config) {
+      console.log('added product: ' + data);
+      self.comment = '';
+      delete self.showProductForm;
+      initiatePage();
+    }).
+    error(function(data, status, headers, config){
+      console.log(data);
+    });
   }
 
   // Controlls the addition of a new comment to a product
@@ -35,6 +55,17 @@ catalogueApp.controller('CatalogueCtrl', [ '$http', function($http) {
   // makes a comment form appear below a product
   this.makeItShow = function (product) {
     product.showForm = true;
+  }
+
+  // retrieve the list of products from the database
+  function initiatePage () {
+    $http.get('/api/').
+      success(getList).
+      error(function(data, status, headers, config){
+      });
+  }
+  function getList (data) {
+    self.products = data.products;
   }
 
   initiatePage();
--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,45 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+
+
+// Connection URL
+var url = 'mongodb://david:ba88el@ds027718.mongolab.com:27718/gerryfudd';
+// Use connect method to connect to the Server
 
 // This data mimics the data that would be stored in a database.
 // If I had more time, I would set up a database and access this information through
 // calls to a database.
-var data = [
-  {'name': 'Product 1',
-   'description': 'This is the first product',
-   'comments': []
-  },
-  {'name': 'Product 2',
-   'description': 'This is the second product',
-   'comments': []
-  },
-  {'name': 'Product 3',
-   'description': 'This is the third product',
-   'comments': []
-  }
-];
+// var data = [
+//   {'name': 'Product 1',
+//    'cost': 3,
+//    'description': 'This is the first product',
+//    'comments': []
+//   },
+//   {'name': 'Product 2',
+//    'cost': 5,
+//    'description': 'This is the second product',
+//    'comments': []
+//   },
+//   {'name': 'Product 3',
+//    'cost': 12,
+//    'description': 'This is the third product',
+//    'comments': []
+//   }
+// ];
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json({"products": data});
+	MongoClient.connect(url, function(err, db) {
+	  assert.equal(null, err);
+	  findProducts(db, function (docs) {
+		  res.json({"products": docs});
+	  	db.close();
+	  });
+	});
+
 });
 
 // adds new comments to an existing product
@@ -31,5 +49,49 @@ router.post('/addComment', function(req, res) {
 	product.comments.push(newComment);
 	res.json(newComment);
 });
+
+// adds new comments to an existing product
+router.post('/addProduct', function(req, res) {
+	var name = req.body.productName;
+	var cost = req.body.productCost;
+	var description = req.body.productDescription;
+	
+	MongoClient.connect(url, function(err, db) {
+	  assert.equal(null, err);
+	  insertProduct(db, [{
+	  	"name": name,
+	  	"cost": cost,
+	  	"description": description,
+	  	"comments": []
+	  }], function (product) {
+		  res.json(product);
+	  	db.close();
+	  });
+	});
+});
+
+function insertProduct (db, data, callback) {
+  // Get the documents collection
+  var collection = db.collection('products');
+  // Insert some documents
+  collection.insert(data, function(err, result) {
+    assert.equal(err, null);
+    console.log("Inserted into the products collection:");
+    console.dir(data);
+    callback(data);
+  });
+}
+
+function findProducts (db, callback) {
+  // Get the documents collection
+  var collection = db.collection('products');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following products:");
+    console.dir(docs);
+    callback(docs);
+  });      
+}
 
 module.exports = router;
